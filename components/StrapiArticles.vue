f92230180235767acc4b2f1c469d51d949c9f6ca
Felix <felix.mangold@adnord.de>
2024-11-11 10:03:47 +0100

    initial

---
+++ components/StrapiArticles.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+const { data, error } = await useAsyncData('index', async () => {
+  const response = await fetch('http://localhost:1337/api/index')
+  return await response.json()
+})
+
+</script>
+
+<template>
+  <div>
+    <h2>Strapi Articles</h2>
+    <pre>
+      {{ data }}
+    </pre>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import StrapiArticles from '~/components/StrapiArticles.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <StrapiArticles />
+  </div>
+</template>
